ddb647b206b164688622229e29e36f81ef5fb21e
anshuL.saini <krishnanshusaini11@gmail.com>
2024-06-19 17:36:21 +0530

    Add SpeedInsights of vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Hero, Highlight, Navbar, Model, Features, HowItWorks, Footer } from '@components';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
 	return (
-		<main className='bg-black overflow-x-hidden'>
-			<Navbar />
-			<Hero />
-			<Highlight />
-			<Model />
-			<Features />
-			<HowItWorks />
-			<Footer />
-		</main>
+		<>
+			<main className='bg-black overflow-x-hidden'>
+				<Navbar />
+				<Hero />
+				<Highlight />
+				<Model />
+				<Features />
+				<HowItWorks />
+				<Footer />
+			</main>
+			<SpeedInsights />
+		</>
 	);
 };
 
